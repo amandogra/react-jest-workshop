d3564936134428bcb1c2e1841692e00005c58452
Aman Dogra <amandogra@gmail.com>
2018-02-08 15:09:29 -0800

    First test related to Button

--- exercises/components/Button.test.js
+++ exercises/components/Button.test.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import {mountToJson} from 'enzyme-to-json';
+import Button from './Button';
 // You're going to need react, mount from enzyme,
 // mountToJson from enzyme-to-json, and ./Button
 
@@ -5,4 +9,9 @@ test('styles the button with a background of the context color', () => {
   // get a new version of the Button component by using stubContext to stub it with the color blue
   // render that component
   // take a snapshot of the result and verify the snapshot
+  const element = mount(<Button children="Click me!" />, {
+    context: {color: 'red'}
+  });
+
+  expect(mountToJson(element)).toMatchSnapshot();
 })
